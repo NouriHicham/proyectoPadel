74c2b7059b3c02af708c23dd407ea5c77122b2db
NouriHicham <nourichamidhicham@fpllefia.com>
2025-03-20 17:13:00 +0100

    header cambiado

--- src/components/main-nav.jsx
+++ src/components/main-nav.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 export function MainNav({ className, ...props }) {
   return (
     <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)} {...props}>
-      <Link to="/" className="text-sm font-medium transition-colors hover:text-primary">
+      <Link to="/" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
         <Home className="h-4 w-4 lg:hidden" />
         <span className="hidden lg:block">Inicio</span>
       </Link>
@@ -17,14 +17,14 @@ export function MainNav({ className, ...props }) {
         <Users2 className="h-4 w-4 lg:hidden" />
         <span className="hidden lg:block">Equipo</span>
       </Link>
-      <Link to="/perfil" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
+      {/* <Link to="/perfil" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
         <User className="h-4 w-4 lg:hidden" />
         <span className="hidden lg:block">Perfil</span>
       </Link>
       <Link to="/configuracion" className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary">
         <Settings className="h-4 w-4 lg:hidden" />
         <span className="hidden lg:block">Configuración</span>
-      </Link>
+      </Link> */}
     </nav>
   )
 }
