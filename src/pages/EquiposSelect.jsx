4ef35bd283da8464825e015af1f6eb38b798a606
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2025-04-10 16:32:39 +0200

    Header en vista equipos select

--- src/pages/EquiposSelect.jsx
+++ src/pages/EquiposSelect.jsx
@@ -7,6 +7,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Home } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "@/supabase/supabase";
+import { Header } from "@/components/header";
 
 export default function EquiposSelect() {
   const [equipos, setEquipos] = useState([]);
@@ -33,7 +34,6 @@ export default function EquiposSelect() {
     fetchEquipos();
   }, []);
 
-
   // Bloque para escuchar cambios en la tabla equipos_personas
   useEffect(() => {
     const changes = supabase
@@ -52,71 +52,77 @@ export default function EquiposSelect() {
     };
   }, []);
 
-
   return (
-    <div className="container relative mx-auto min-h-screen flex flex-col justify-center items-center">
-      <Button className="absolute right-4 top-10" onClick={signOut}>
-        Cerrar Sesión
-      </Button>
-      {equipoPersona && (
-        <Button
-          className="fixed right-3 bottom-5 rounded-full"
-          onClick={() => navigate("/")}
+    <>
+      {equipoPersona && <Header />}
+      <div className="container relative mx-auto min-h-screen flex flex-col justify-center items-center py-4">
+        {/* <Button className="absolute right-4 top-10" onClick={signOut}>
+          Cerrar Sesión
+        </Button> */}
+        {equipoPersona && (
+          <Button
+            className="fixed right-3 bottom-5 rounded-full"
+            onClick={() => navigate("/")}
+          >
+            <Home className="" />
+          </Button>
+        )}
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold">Mis equipos</h1>
+          <p>Visualiza los equipos a los que perteneces o tus invitaciones</p>
+        </div>
+        <Tabs
+          defaultValue="teams"
+          className="my-5 flex flex-col items-center justify-center"
         >
-          <Home className="" />
-        </Button>
-      )}
-      <div className="text-center">
-        <h1 className="text-4xl font-semibold">Mis equipos</h1>
-        <p>Visualiza los equipos a los que perteneces o tus invitaciones</p>
-      </div>
-      <Tabs
-        defaultValue="teams"
-        className="my-5 flex flex-col items-center justify-center"
-      >
-        <TabsList>
-          <TabsTrigger value="teams" className={"cursor-pointer"}>
-            Equipos
-          </TabsTrigger>
-          <TabsTrigger value="invitations" className={"cursor-pointer"}>
-            Invitaciones
-          </TabsTrigger>
-          <TabsTrigger value="requests" className={"cursor-pointer"}>
-            Solicitar
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value="teams">
-          {/* Aceptado, Invitado, Pendiente */}
+          <TabsList>
+            <TabsTrigger value="teams" className={"cursor-pointer"}>
+              Equipos
+            </TabsTrigger>
+            <TabsTrigger value="invitations" className={"cursor-pointer"}>
+              Invitaciones
+            </TabsTrigger>
+            <TabsTrigger value="requests" className={"cursor-pointer"}>
+              Solicitar
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="teams">
+            {/* Aceptado, Invitado, Pendiente */}
 
-          {/* Equipos a los que perteneces */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-            {equipos
-              .filter((equipo) => equipo.estado == "aceptado")
-              .map((equipo) => (
-                <CardEquipo key={equipo.id} equipo={equipo} />
+            {/* Equipos a los que perteneces */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              {equipos
+                .filter((equipo) => equipo.estado == "aceptado")
+                .map((equipo) => (
+                  <CardEquipo key={equipo.id} equipo={equipo} />
+                ))}
+            </div>
+          </TabsContent>
+          <TabsContent value="invitations">
+            {/*Equipos que te han invitado */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              {equipos
+                .filter((equipo) => equipo.estado == "invitado")
+                .map((equipo) => (
+                  <CardEquipo
+                    key={equipo.id}
+                    equipo={equipo}
+                    invitation={true}
+                  />
+                ))}
+            </div>
+          </TabsContent>
+          <TabsContent value="requests">
+            {/* Pestaña para solicitar unión a equipo */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              {/* Filtrar por persona id */}
+              {equiposDiferentes.map((equipo) => (
+                <CardEquipo key={equipo.id} equipo={equipo} solicitar={true} />
               ))}
-          </div>
-        </TabsContent>
-        <TabsContent value="invitations">
-          {/*Equipos que te han invitado */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-            {equipos
-              .filter((equipo) => equipo.estado == "invitado")
-              .map((equipo) => (
-                <CardEquipo key={equipo.id} equipo={equipo} invitation={true} />
-              ))}
-          </div>
-        </TabsContent>
-        <TabsContent value="requests">
-          {/* Pestaña para solicitar unión a equipo */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-            {/* Filtrar por persona id */}
-            {equiposDiferentes.map((equipo) => (
-              <CardEquipo key={equipo.id} equipo={equipo} solicitar={true} />
-            ))}
-          </div>
-        </TabsContent>
-      </Tabs>
-    </div>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 }
