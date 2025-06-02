06461de6d78d43579516d2857e6ea11ac64a8d62
NouriHicham <nourichamidhicham@fpllefia.com>
2025-06-02 16:45:04 +0200

    arreglo imagen

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -23,7 +23,7 @@ export default function LoginPage() {
         </div>
         <div className="relative hidden bg-muted lg:block">
           <img
-            src="src/images/padelInicio.jpg"
+            src="/padelInicio.jpg"
             alt="Image"
             className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
           />
